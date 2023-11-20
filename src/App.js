c4485df947851eff0bb23dc56b8ba563ace9d299
Amauri <hamauri@aol.com>
2023-11-19 16:12:32 -0800

    added router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import { Game, GameOver, Home, ScoreBoard } from './pages';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <ul style={{ listStyle: 'none' }}>
+            <li>
+              <Link to={''}>Home</Link>
+            </li>
+            <li>
+              <Link to={'/game'}>Game</Link>
+            </li>
+            <li>
+              {' '}
+              <Link to={'game-over'}>Game Over</Link>
+            </li>
+            <li>
+              <Link to={'score-board'}>Score board</Link>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<Game />} />
+          <Route path="/game-over" element={<GameOver />} />
+          <Route path="/score-board" element={<ScoreBoard />} />
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ src/pages/ScoreBoard/index.js
@@ -0,0 +1,5 @@
+const ScoreBoard = () => {
+  return <p>ScoreBoard</p>;
+};
+
+export default ScoreBoard;
