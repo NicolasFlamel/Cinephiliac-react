dff46c5d10db93843446424d3f842543304fb9d7
Amauri <hamauri@aol.com>
2023-11-24 15:51:25 -0800

    fetchData() updated

---
+++ src/components/Movie.js
@@ -0,0 +1,12 @@
+const Movie = ({ movieData }) => {
+  // console.log(movieData);
+  return (
+    <section className="movie-card">
+      <h2 className="game-option"></h2>
+      <img className="movie-poster" src="" />
+      <h2 className="movie-name"></h2>
+    </section>
+  );
+};
+
+export default Movie;
--- src/pages/Game/index.js
+++ src/pages/Game/index.js
@@ -2,14 +2,15 @@ import { useEffect, useState } from 'react';
 import './styles.css';
 // testing purpose, remove later
 import temp from './temp.json';
+import Movie from '../../components/Movie';
 
 const Game = ({ gameType, gameGenre }) => {
   const [movieList, setMovieList] = useState(
     JSON.parse(localStorage.getItem(`${gameGenre}`)) || [],
   );
+  const [firstMovie, setFirstMovie] = useState(null);
+  const [secondMovie, setSecondMovie] = useState(null);
   // get movie data. maybe props or use components
-  const firstMovie = { title: 'first' };
-  const secondMovie = { title: 'second' };
   const options = {
     method: 'GET',
     headers: {
@@ -21,33 +22,79 @@ const Game = ({ gameType, gameGenre }) => {
   useEffect(() => {
     // fetch game api
     const fetchData = async (next) => {
-      // const url =
-      //   'https://moviesdatabase.p.rapidapi.com' +
-      //   (next ? next : '/titles?startYear=2000&list=top_rated_english_250');
+      const url =
+        'https://moviesdatabase.p.rapidapi.com' +
+        (next ? next : '/titles?startYear=2000&list=top_rated_english_250');
 
-      // const response = await fetch(url, options);
-      // const data = await response.json();
-      // const movieList = [...data.results];
-      // const fullData = data.next ? fetchData(data.next) : [];
+      const response = await fetch(url, options);
+      const data = await response.json();
+      const resultsList = [...data.results];
+      const nextData = data.next ? await fetchData(data.next) : [];
+      const fullList = resultsList.concat(nextData);
 
-      // return movieList.concat(fullData);
-      console.log('fetch', temp);
-      localStorage.setItem(`${gameGenre}`, JSON.stringify(temp));
-      setMovieList(temp);
-      return temp;
+      return fullList;
+      // console.log('fetch', temp);
+      // localStorage.setItem(`${gameGenre}`, JSON.stringify(temp));
+      // setMovieList(temp);
+      // return temp;
     };
 
-    if (!movieList.length) fetchData().catch(console.error);
+    if (!movieList.length) {
+      fetchData()
+        .then((fetchedMovieList) => {
+          localStorage.setItem(
+            `${gameGenre}`,
+            JSON.stringify(fetchedMovieList),
+          );
+          setMovieList(fetchedMovieList);
+        })
+        .catch(console.error);
+    }
   }, [gameType, gameGenre]);
+
+  if (movieList.length === 0) {
+    return <h2>Fetching Data...</h2>;
+  } else if (!firstMovie && !secondMovie) {
+    setFirstMovie(
+      movieList[Math.floor(Math.random() * (movieList.length - 1))],
+    );
+    setSecondMovie(
+      movieList[Math.floor(Math.random() * (movieList.length - 1))],
+    );
+
+    return <h2>Loading Movies...</h2>;
+  }
 
   return (
     <section id="game-section">
       <section id="question">
-        <em>{secondMovie.title}</em> has a higher or lower {gameType} amount
-        than <em>{firstMovie.title}</em>?
+        <em>{secondMovie.originalTitleText.text}</em> has a higher or lower{' '}
+        {gameType} amount than <em>{firstMovie.originalTitleText.text}</em>?
       </section>
-      <article id="movie-one"></article>
-      <article id="movie-two"></article>
+
+      <section className="game">
+        <article id="movie-one">
+          <Movie movieData={firstMovie} />
+        </article>
+
+        <article id="movie-two">
+          <Movie movieData={secondMovie} />
+        </article>
+
+        <section id="higher-lower-btns">
+          <div className="ui buttons">
+            <button type="button" value="higher" className="ui button positive">
+              Higher
+            </button>
+
+            <div className="or"></div>
+
+            <button type="button" value="lower" className="ui button negative">
+              Lower
+            </button>
+          </div>
+        </section>
+      </section>
     </section>
   );
 };
