93554240d7802c0f65a6cc39fc9c9cf8e1042f40
Amauri <hamauri@aol.com>
2023-12-13 11:19:23 -0800

    adding old code

--- src/pages/GameOver/index.js
+++ src/pages/GameOver/index.js
@@ -6,6 +6,28 @@ const GameOver = ({ gameMode, gameGenre, score }) => {
         <li>gameGenre: {gameGenre}</li>
         <li>score: {score.current}</li>
       </ul>
+      <section id="score">
+        {/* display results */}
+        <h2>
+          You're score was: <span class="results"></span>
+        </h2>
+        <h3>
+          Game mode: <span class="results"></span>
+        </h3>
+        <h3>
+          Genre: <span class="results"></span>
+        </h3>
+      </section>
+      <section id="user-info">
+        {/* user inputs info for scoreboard */}
+        <h2>Enter Player Name:</h2>
+        <div class="ui input">
+          <input class="username-input" type="text" placeholder="Name" />
+          <button class="custom-btn" id="save-btn" type="button">
+            Save
+          </button>
+        </div>
+      </section>
     </div>
   );
 };
