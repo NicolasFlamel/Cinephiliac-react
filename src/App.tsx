6cac3a26ba5926d636984a9ed97d70d366255fdb
Amauri <hamauri@aol.com>
2024-04-24 12:46:58 -0700

    tailwind/nextui setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { Header } from './components';
-import { Game, GameOver, Home, ScoreBoard } from './pages';
 import { useRef, useState } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Game, GameOver, Home, ScoreBoard } from './pages';
+import { Header } from './components';
+import { NextUIProvider } from '@nextui-org/react';
 
 function App() {
   const [gameMode, setGameMode] = useState('Box-Office');
@@ -10,37 +11,39 @@ function App() {
   const score = useRef<number>(0);
 
   return (
-    <div className="App">
-      <Header />
-      <main>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Home setGameMode={setGameMode} setGameGenre={setGameGenre} />
-            }
-          />
-          <Route
-            path="/game"
-            element={
-              <Game gameMode={gameMode} gameGenre={gameGenre} score={score} />
-            }
-          />
-          <Route
-            path="/game-over"
-            element={
-              <GameOver
-                gameMode={gameMode}
-                gameGenre={gameGenre}
-                score={score}
-              />
-            }
-          />
-          <Route path="/score-board" element={<ScoreBoard />} />
-          <Route path="/*" element={<Navigate to="/" />} />
-        </Routes>
-      </main>
-    </div>
+    <NextUIProvider>
+      <div className="App">
+        <Header />
+        <main>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Home setGameMode={setGameMode} setGameGenre={setGameGenre} />
+              }
+            />
+            <Route
+              path="/game"
+              element={
+                <Game gameMode={gameMode} gameGenre={gameGenre} score={score} />
+              }
+            />
+            <Route
+              path="/game-over"
+              element={
+                <GameOver
+                  gameMode={gameMode}
+                  gameGenre={gameGenre}
+                  score={score}
+                />
+              }
+            />
+            <Route path="/score-board" element={<ScoreBoard />} />
+            <Route path="/*" element={<Navigate to="/" />} />
+          </Routes>
+        </main>
+      </div>
+    </NextUIProvider>
   );
 }
 
