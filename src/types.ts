684c9a603b151fbbbfd22972f7820290c4685cc5
Amauri <hamauri@aol.com>
2024-04-25 12:19:49 -0700

    updated types

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,19 @@
 import { Dispatch, MutableRefObject, SetStateAction } from 'react';
 
+const gameModes = ['Box-Office', 'Ratings'] as const;
+const genres = [
+  'All-Genres',
+  'Action',
+  'Animation',
+  'Comedy',
+  'Crime',
+  'Family',
+  'Horror',
+  'Romance',
+  'Sci-Fi',
+  'Thriller',
+] as const;
+
 export interface Movie {
   imdbId: string;
   genre?: Array<string>;
@@ -10,17 +24,29 @@ export interface Movie {
 }
 
 export interface GameProps {
-  gameMode: string;
-  gameGenre: string;
+  gameMode: GameModeType;
+  gameGenre: GameGenreType;
   score: MutableRefObject<number>;
 }
 
 export interface ScoreData {
   id: string;
-  gameMode: string;
-  gameGenre: string;
+  gameMode: GameModeType;
+  gameGenre: GameGenreType;
   score: number;
   username: string;
 }
 
+export type GameModeType = (typeof gameModes)[number];
+
+export type GameGenreType = (typeof genres)[number];
+
 export type Dispatcher<T> = Dispatch<SetStateAction<T>>;
+
+export const isGameModeType = (value: string): value is GameModeType => {
+  return gameModes.includes(value as GameModeType);
+};
+
+export const isGameGenreType = (value: string): value is GameGenreType => {
+  return genres.includes(value as GameGenreType);
+};
