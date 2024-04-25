472048a9a373ebde25d215b3d6719140ef235c19
Amauri <hamauri@aol.com>
2024-04-25 14:16:17 -0700

    updated MovieCard with NextUI
    MovieCard now uses Image component form NextUI

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,33 +1,17 @@
-import { useEffect, useState } from 'react';
-import noImg from 'assets/img/no-image-placeholder.png';
 import { Movie } from 'types';
+import { Image } from '@nextui-org/react';
+import noImg from 'assets/img/no-image-placeholder.png';
 
 const MovieCard = ({ movieData }: { movieData: Movie }) => {
-  const [imgLoaded, setImgLoaded] = useState(false);
-
-  useEffect(() => {
-    const img = new Image();
-    img.onload = () => {
-      setImgLoaded(true);
-    };
-    if (movieData.posterUrl) img.src = movieData.posterUrl;
-  }, [movieData.posterUrl]);
-
   return (
     <>
-      <div className={imgLoaded ? 'movie-poster' : 'img-placeholder'}>
-        {imgLoaded ? (
-          <img
-            src={movieData.posterUrl}
-            alt={movieData.title + ' poster'}
-            onError={({ currentTarget }) => {
-              currentTarget.onerror = null;
-              currentTarget.src = noImg;
-            }}
-          />
-        ) : (
-          <span>Loading</span>
-        )}
+      <div>
+        <Image
+          width={300}
+          height={400}
+          src={movieData.posterUrl || noImg}
+          alt={movieData.title + ' poster'}
+        />
       </div>
       <h2>{movieData.title}</h2>
     </>
