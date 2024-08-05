5da1277df251e597363928e24c1010fbd80b7622
Amauri <hamauri@aol.com>
2024-08-05 11:36:12 -0700

    updated assets

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,8 @@
 import { ChangeEvent, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Dispatcher } from 'types';
-import { ReactComponent as Sun } from 'assets/img/sun.svg';
-import { ReactComponent as Moon } from 'assets/img/moon.svg';
+import Sun from 'assets/img/sun.svg';
+import Moon from 'assets/img/moon.svg';
 import {
   Link,
   Navbar,
@@ -23,8 +23,9 @@ interface HeaderProps extends React.HTMLAttributes<HTMLElement> {
 type UpdateThemeParam = ChangeEvent<HTMLInputElement>;
 
 const Header = ({ darkMode, setDarkMode }: HeaderProps) => {
-  let location = useLocation();
+  const location = useLocation();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const svgStyle: React.CSSProperties = { width: '1em' };
 
   const updateTheme = ({ currentTarget }: UpdateThemeParam) => {
     const { checked } = currentTarget;
@@ -65,8 +66,8 @@ const Header = ({ darkMode, setDarkMode }: HeaderProps) => {
           <Switch
             size="lg"
             isSelected={darkMode}
-            startContent={<Sun fill="yellow" />}
-            endContent={<Moon fill="black" />}
+            startContent={<img src={Sun} style={svgStyle} />}
+            endContent={<img src={Moon} style={svgStyle} />}
             onChange={updateTheme}
           />
         </NavbarItem>
@@ -92,8 +93,8 @@ const Header = ({ darkMode, setDarkMode }: HeaderProps) => {
           <Switch
             size="lg"
             isSelected={darkMode}
-            startContent={<Sun fill="yellow" />}
-            endContent={<Moon fill="black" />}
+            startContent={<img src={Sun} style={svgStyle} />}
+            endContent={<img src={Moon} style={svgStyle} />}
             onChange={updateTheme}
           />
         </NavbarMenuItem>
