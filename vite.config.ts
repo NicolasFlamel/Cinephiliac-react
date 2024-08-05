69624398587ca64f40d2aeadc90e656d01c13970
Amauri <hamauri@aol.com>
2024-08-05 10:48:31 -0700

    updated config files

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import checker from 'vite-plugin-checker';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    checker({
+      typescript: {
+        tsconfigPath: 'tsconfig.app.json',
+      },
+      overlay: true,
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+});
